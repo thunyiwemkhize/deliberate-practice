f9a4b00ac6ac180cdd91f82d46acd06edc0aa431
Thunyiwe Mkhize <thunyiwe.mkhize@chillisoft.co.za>
2023-06-02 16:20:14 +0200

    still working on assessment of symfony

--- client/src/Controller/LuckyController.php
+++ client/src/Controller/LuckyController.php
@@ -16,10 +16,6 @@ class LuckyController extends AbstractController
     #[Route('/lucky/number')]
     public function number(): Response
     {
-        $number = random_int(0, 100);
-
-        return $this->render('lucky/number.html.twig', [
-            'number' => $number,
-        ]);
+        return new Response(random_int(0, 100));
     }
 }
--- client/tests/ControllerTests/LuckyControllerTest.php
+++ client/tests/ControllerTests/LuckyControllerTest.php
@@ -3,24 +3,55 @@ declare(strict_types=1);
 
 namespace App\Tests\ControllerTests;
 use App\Controller\LuckyController;
-use Exception;
+
 use PHPUnit\Framework\TestCase;
 
 class LuckyControllerTest extends TestCase
 {
-    /**
-     * @throws Exception
-     */
+    public function testLuckyNumberResponseIsOK(): void
+    {
+//        Arrange
+        $luckyNumber = $this->makeLuckyNumberController();
+
+//        Act
+        $actual = $luckyNumber->number()->getStatusCode();
+
+//        Assert
+        $this->assertEquals(200,$actual);
+    }
     public function testLuckyIsNotEmpty(): void
     {
-//        sut
+//        Arrange
         $luckyNumber = $this->makeLuckyNumberController();
 
 //        Act
         $actual = $luckyNumber->number();
 
 //        Assert
-        $this->assertNotEmpty(strval($actual));
+        $this->assertNotEmpty($actual);
+    }
+    public function testLucyNumberRange(): void
+    {
+//        Arrange
+        $luckyNumber = $this->makeLuckyNumberController();
+
+//        Act
+        $actual = $luckyNumber->number();
+
+//        Assert
+        $this->assertIsInt((int)$actual->getContent());
+    }
+    public function testGiven100Max_ShouldReturnNumberBetween0And100(): void
+    {
+//        Arrange
+        $luckyNumber = $this->makeLuckyNumberController();
+
+//        Act
+        $actual =(int) $luckyNumber->number()->getContent();
+
+//        Assert
+        $this->assertGreaterThanOrEqual(0, $actual);
+        $this->assertLessThanOrEqual(100, $actual);
     }
 
     public function makeLuckyNumberController(): LuckyController
